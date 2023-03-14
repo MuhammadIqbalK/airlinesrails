c9d12e8b54dbd5583f2ac158f46eb927b40dbbad
tangguhw <20102186@ittelkom-pwt.ac.id>
2023-03-15 01:29:09 +0700

    creating schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_14_114321) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_163421) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "airlines", force: :cascade do |t|
+    t.string "name"
+    t.integer "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "airports", force: :cascade do |t|
+    t.string "name"
+    t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "flights", force: :cascade do |t|
+    t.integer "departure_airport"
+    t.integer "arrival_airport"
+    t.datetime "departure_time"
+    t.datetime "arrival_time"
+    t.integer "price"
+    t.integer "create_table"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.integer "total_passenger"
+    t.integer "price"
+    t.integer "flight_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
